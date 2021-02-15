c1413c965e6d2d242d1085acb91b41e450f17b77
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2021-02-14 17:37:21 -0800

    added my users router and model

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,17 @@
 const express = require("express")
 
+const db = require("./users-model");
+const restricted = require("../auth/restricted-middleware");
+
 const router = express.Router();
+
+router.get("/", restricted, async (req, res, next) => {
+    try {
+        const users = await db.find();
+    } catch(error) {
+        next({ apiCode: 500, apiMessage: 'You shall not pass', ...err })
+    }
+})
 
 
 module.exports = router;
